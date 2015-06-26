e6ee344a719c201ea9652338e3ce904847abbc31
aruprakshit <aruprakshit@rocketmail.com>
2015-06-26 19:03:05 +0530

    ...

--- app/assets/javascripts/hotel.js
+++ app/assets/javascripts/hotel.js
@@ -1,16 +1,16 @@
 $(function() {
   $('#hotel_booking_date').datepick({ 
-    rangeSelect: true, dateFormat: 'yyyy-mm-dd', showTrigger: '#calImg'
+    multiSelect: 4, dateFormat: 'yyyy-mm-dd', showTrigger: '#calImg'
   });
 
   $.get( "/admin/hotels/fetch_dates", { package_type: 'weekend' } )
   .done(function( data ) {
     console.log( data.dates );
-    $('#hotel_booking_date').datepick({
-      beforeShowDay: function(date){
-        var string = jQuery.datepicker.formatDate('yyyy-mm-dd', date);
-        console.log(data.dates.indexOf(string));
-        return [ data.dates.indexOf(string) != -1 ]
+    $('.date-picker').datepick({
+      onDate: function(date, current){
+        var date_str = $.datepick.formatDate('yyyy-mm-dd', date);
+        console.log(data.dates.indexOf(date_str));
+        return {selectable: ($.inArray(date_str,data.dates) === -1)};
       }
     });
   });
