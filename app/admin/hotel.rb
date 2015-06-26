1ac1e5b2f6be3d72e1423760d5cb72158d1d500d
aruprakshit <aruprakshit@rocketmail.com>
2015-06-26 18:27:14 +0530

    ...

--- app/admin/hotel.rb
+++ app/admin/hotel.rb
@@ -13,8 +13,43 @@ ActiveAdmin.register Hotel do
   #   permitted
   # end
   #
-  
+
   permit_params :booking_date, :name
+
+  controller do
+    private
+
+    def package_dates pkg_type
+      case pkg_type
+      when "weekend"
+        collect_dates Date.today, Date.today + 1.year, 'THURSDAY'
+      when "full_week"
+        collect_dates Date.today, Date.today + 1.year, 'THURSDAY', 'SUNDAY'
+      when "midweek"
+        collect_dates Date.today, Date.today + 1.year, 'SUNDAY'
+      end
+    end
+
+    def interval pkg_type
+      case pkg_type
+      when 'weekend'
+        3
+      when'midweek'
+        4
+      when 'full_week'
+        7
+      end
+    end
+
+    def collect_dates start_date, end_date, *days
+      (start_date..end_date).select { |date| days.include? date.strftime("%^A") }
+    end
+  end
+
+  collection_action :fetch_dates, method: :get do
+    render :json => { dates: package_dates(params['package_type']), interval: interval(params['package_type'])}
+  end
+
 
   form do |f|
     inputs 'Details' do
--- app/assets/javascripts/hotel.js
+++ app/assets/javascripts/hotel.js
@@ -1,3 +1,17 @@
 $(function() {
-  $('#hotel_booking_date').datepick();
+  $('#hotel_booking_date').datepick({ 
+    rangeSelect: true, dateFormat: 'yyyy-mm-dd', showTrigger: '#calImg'
+  });
+
+  $.get( "/admin/hotels/fetch_dates", { package_type: 'weekend' } )
+  .done(function( data ) {
+    console.log( data.dates );
+    $('#hotel_booking_date').datepick({
+      beforeShowDay: function(date){
+        var string = jQuery.datepicker.formatDate('yyyy-mm-dd', date);
+        console.log(data.dates.indexOf(string));
+        return [ data.dates.indexOf(string) != -1 ]
+      }
+    });
+  });
 });
