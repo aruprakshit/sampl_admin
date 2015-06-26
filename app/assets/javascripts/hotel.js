cf0f6b6666c4e79bc6bcb9ad3090eafe572eef13
aruprakshit <aruprakshit@rocketmail.com>
2015-06-26 19:18:16 +0530

    ...

--- app/assets/javascripts/hotel.js
+++ app/assets/javascripts/hotel.js
@@ -1,17 +1,17 @@
 $(function() {
-  $('#hotel_booking_date').datepick({ 
-    multiSelect: 4, dateFormat: 'yyyy-mm-dd', showTrigger: '#calImg'
-  });
 
   $.get( "/admin/hotels/fetch_dates", { package_type: 'weekend' } )
   .done(function( data ) {
     console.log( data.dates );
-    $('.date-picker').datepick({
+    $('#hotel_booking_date').datepick({
+      dateFormat: 'yyyy-mm-dd',
+      multiSelect: 4,
       onDate: function(date, current){
         var date_str = $.datepick.formatDate('yyyy-mm-dd', date);
         console.log(data.dates.indexOf(date_str));
-        return {selectable: ($.inArray(date_str,data.dates) === -1)};
-      }
+        return {selectable: ($.inArray(date_str,data.dates) != -1)};
+      },
+      showTrigger: '#calImg'
     });
   });
 });
