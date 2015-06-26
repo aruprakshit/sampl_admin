0253089090f776c13d7082f1cdb09f1f420de3d4
Arup Rakshit <aruprakshit@rocketmail.com>
2015-06-27 02:07:25 +0530

    ..

--- app/assets/javascripts/hotel.js
+++ app/assets/javascripts/hotel.js
@@ -1,17 +1,18 @@
 $(function() {
-
   $.get( "/admin/hotels/fetch_dates", { package_type: 'weekend' } )
   .done(function( data ) {
-    console.log( data.dates );
     $('#hotel_booking_date').datepick({
       dateFormat: 'yyyy-mm-dd',
-      multiSelect: 4,
+      multiSelect: data.interval,
       onDate: function(date, current){
         var date_str = $.datepick.formatDate('yyyy-mm-dd', date);
-        console.log(data.dates.indexOf(date_str));
         return {selectable: ($.inArray(date_str,data.dates) != -1)};
       },
-      showTrigger: '#calImg'
+      showTrigger: '#calImg',
+      onSelect: function(dates) {
+        console.log(dates);
+        $(this).datepick('setDate', [dates[0], date.interval]);
+      }
     });
   });
 });
