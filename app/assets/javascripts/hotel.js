dbb27f23c713863323c68ba9d3dfde386aaa7949
Arup Rakshit <aruprakshit@rocketmail.com>
2015-06-27 12:18:22 +0530

    Working dateselection code...

--- app/assets/javascripts/hotel.js
+++ app/assets/javascripts/hotel.js
@@ -3,16 +3,26 @@ $(function() {
   .done(function( data ) {
     $('#hotel_booking_date').datepick({
       dateFormat: 'yyyy-mm-dd',
-      multiSelect: data.interval,
-      onDate: function(date, current){
-        var date_str = $.datepick.formatDate('yyyy-mm-dd', date);
-        return {selectable: ($.inArray(date_str,data.dates) != -1)};
-      },
-      showTrigger: '#calImg',
-      onSelect: function(dates) {
-        console.log(dates);
-        $(this).datepick('setDate', [dates[0], date.interval]);
+    multiSelect: data.interval,
+    onDate: function(date, current){
+      var date_str = $.datepick.formatDate('yyyy-mm-dd', date);
+      return {selectable: ($.inArray(date_str,data.dates) != -1)};
+    },
+    showTrigger: '#calImg',
+    onSelect: function(dates) {
+      console.log(dates);
+      var seldate = new Date(dates[0]);
+      seldates = [];
+      var count = 0;
+      while ( count < data.interval ) {
+        date = new Date();
+        date.setDate(seldate.getDate() + count +1);
+        seldates.push(date);
+        count++;
       }
+      $(this).datepick('setDate', seldates);
+      console.log(seldates);
+    }
     });
   });
 });
